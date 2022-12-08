6de1d2eecffa14f806ed8ca16deba14576d9addf
cufigenti <92585818+cufigenti@users.noreply.github.com>
2022-12-08 19:26:32 +0100

    Add files via upload

---
+++ babakingi3-main/src/App.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+
+function App() {
+  return (
+    <><React.Fragment>
+      <Navbar />
+    </React.Fragment><div>
+      
+        <div className='hero'>
+
+          <h1>test be</h1>
+          <p className='p1'>test be e vogel</p>
+          <a href='https://lutfimuaremi.info/' className='b1'>click !</a>
+        </div>
+        <div className='nav2'>
+          <div className='text2'>
+            <h1>test be1</h1>
+            <p>attention to details ...</p>
+          </div>
+          <div className='outer-links'>
+            <div className='linkat'>
+              <ul id='linkat-horizontal'>
+                <li><a className='a1' href='#'>Show all</a></li>
+                <li><a className='a2' href='#'>Branding</a></li>
+                <li><a className='a3' href='#'>Creative</a></li>
+                <li><a className='a4' href='#'>Design</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='nav'>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+          <img width="29.3%" height="349px" src='img3.jpg'></img>
+        </div>
+        <div class="parent">
+          <div class="div1"> cufiiiii </div>
+          <div class="div2"> Let's Talk
+            <p> +389 078-317-223</p>
+            <a href=''>babakingi3@mail2tor.com</a>
+          </div>
+          <div class="div3"> Find Us
+            <p>Ilindenska 121/4 Gostivar</p>
+            <p>32000 Gostivar</p>
+          </div>
+          <div className='wrap-div4'>
+            <div class="div4"> Follow us </div>
+
+            <div className='icons'>
+              <i><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="3" y="5" width="18" height="14" rx="4"></rect>
+                <path d="M10 9l5 3l-5 3z"></path>
+              </svg></i>
+              <i><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+              </svg></i>
+              <i><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
+              </svg></i>
+              <i><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="4" y="4" width="16" height="16" rx="4"></rect>
+                <circle cx="12" cy="12" r="3"></circle>
+                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
+              </svg></i>
+              <i><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="4" y="4" width="16" height="16" rx="2"></rect>
+                <line x1="8" y1="11" x2="8" y2="16"></line>
+                <line x1="8" y1="8" x2="8" y2="8.01"></line>
+                <line x1="12" y1="16" x2="12" y2="11"></line>
+                <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+              </svg></i>
+            </div>
+          </div>
+        </div>
+
+      </div></>
+  )
+
+
+  
+
+
+
+
+
+
+}
+
+export default App;
